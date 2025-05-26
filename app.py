e57b97f2b16c8a09b4eda99a6eb3b5816ec4bfa6
JD64Bits <jesusolarte121206@gmail.com>
2025-05-26 16:20:04 -0500

    feat: :sparkles: Pequeños Cambio

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 import json
 import socket
 from werkzeug.utils import secure_filename
+import bcrypt
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
@@ -909,7 +910,7 @@ def crear_venta():
 @app.route("/api/cargar/usuarios", methods = ["GET"])
 def cargar_usuarios():
     usuarios = conn_db.seleccionar(tabla = "usuarios",
-                                   columnas= "id, nombre, rol")
+                                   columnas= "id, usuario, rol")
     if usuarios:        
         lista_usuarios = [{"id":id, "nombre":nombre, "rol":rol} for id, nombre, rol in usuarios]
         return jsonify(lista_usuarios), 200
@@ -923,10 +924,12 @@ def cargar_usuarios():
 @app.route("/api/new_usuario", methods = ["POST"])
 def new_usuario():
     datos = request.get_json()
+    datos["contrasena"] = bcrypt.hashpw(datos["contrasena"].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
     print(datos)
     id_usuario = conn_db.insertar(tabla="usuarios", datos=datos)
+    print(id_usuario)
     if id_usuario:        
-        usuario = [{"id":id_usuario, "nombre":datos["nombre"], "rol":datos["rol"]}]
+        usuario = [{"id":id_usuario, "usuario":datos["usuario"], "rol":datos["rol"]}]
         return jsonify(usuario), 200
     else:
         return jsonify({
